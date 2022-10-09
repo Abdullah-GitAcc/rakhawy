ebcf1e5f81c6ad6329fdd8c38083af3cd1bc55f6
raeadgaa444 <raeadgaa444@hotmail.com>
2022-10-09 11:42:41 +0300

    Timer
    In-game Timer with minutes and seconds displayed

--- Rakhawy/Assets/Scripts/UIManager.cs
+++ Rakhawy/Assets/Scripts/UIManager.cs
@@ -5,17 +5,46 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
-    private Text _score;
+    private Text _score, _time;
+    private float _timer = 0;
+    private float _seconds;
+    private float _minutes;
+    private string MinutesString = "";
+    private string SecondString = "";
     // Start is called before the first frame update
     void Start()
     {
         _score.text = "Score: " + 0;
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timer += Time.deltaTime;
+         _minutes = Mathf.Floor(_timer / 59);
+        _seconds = Mathf.RoundToInt(_timer % 59);
+
+    
+
+        if (_minutes < 10)
+        {
+            MinutesString = "0" + _minutes.ToString();
+        }
+        else
+        {
+            MinutesString = _minutes.ToString();
+        }
+        if (_seconds < 10)
+        {
+            SecondString = "0" + Mathf.RoundToInt(_seconds).ToString();
+        }
+        else
+        {
+            SecondString = _seconds.ToString();
+        }
+
+        _time.text = "Time : " + MinutesString + ":"+ SecondString;        
     }
     public void ViewScore(int Score)
     {
